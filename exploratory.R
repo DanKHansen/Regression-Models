bf474c8f1ecfcb95120992d7a3da5553cfb1a495
DanKHansen <dankh@live.dk>
2016-12-12 23:28:18 +0100

    stuff

--- exploratory.R
+++ exploratory.R
@@ -18,7 +18,7 @@ summary(ds)
 pairs(ds, col=ds$am, panel = panel.smooth)
 
 #boxplot(factor(ds$am, labels = c('a','m')),ds$mpg,col=c('lightgreen','lightblue'),xlab='transmission', ylab='mpg')
-plot(as.factor(ds$am),ds$mpg,col=c('lightgreen','lightblue'),xlab='transmission', ylab='mpg')
+plot(as.factor(ds$am),ds$mpg,col=c('lightgreen','lightblue'), xlab='transmission', ylab='mpg')
 abline(h = mean(ds$mpg[ds$am==0]),col='red')
 abline(h = mean(ds$mpg[ds$am==1]),col='red')
 
@@ -31,25 +31,28 @@ summary(mdl)
 summary(mdl2)
 
 par.orig <- par()
-
+cols <- c(ds$am)
 par(mfrow = c(2, 2), oma = c(0, 0, 2, 0))
-plot(fit,col=ds$am,pch=19)
-plot(mdl,col=ds$am,pch=19)
-plot(mdl2,col=ds$am,pch=19)
+plot(fit,col=alpha(cols,alpha=.5),pch=19)
+
+plot(mdl,col=alpha(cols,alpha=.5),pch=19)
+plot(mdl2,col=alpha(cols,alpha=.5),pch=19)
 par(par.orig)
 
 z <- lm(mpg ~ disp,ds)
-plot(ds$disp,ds$mpg,pch=19,col=ds$am,xlab='disp', ylab='mpg')
+plot(ds$disp,ds$mpg,pch=19,col=alpha(cols,alpha=.5),xlab='disp', ylab='mpg')
 abline(z,col='blue')
 
-plot(ds$hp,ds$mpg,pch=19,col=ds$am,xlab='hp', ylab='mpg')
-plot(ds$drat,ds$mpg,pch=19,col=ds$am,xlab='drat', ylab='mpg')
+
+
+plot(ds$hp,ds$mpg,pch=19,col=alpha(cols,alpha=.5),xlab='hp', ylab='mpg')
+plot(ds$drat,ds$mpg,pch=19,col=alpha(cols,alpha=.5),xlab='drat', ylab='mpg')
 
 z <- lm(mpg ~ wt,ds)
-plot(ds$wt,ds$mpg,pch=19,col=ds$am,xlab='weight', ylab='mpg')
+plot(ds$wt,ds$mpg,pch=19,col=alpha(cols,alpha=.5),xlab='weight', ylab='mpg')
 abline(z,col='blue')
 
-plot(ds$qsec,ds$mpg,pch=19,col=ds$am,xlab='1/4 mile', ylab='mpg')
+plot(ds$qsec,ds$mpg,pch=19,col=alpha(cols,alpha=.5),xlab='1/4 mile', ylab='mpg')
 
 plot(ds$vs,ds$mpg,pch=19,col='lightgreen',xlab='vs', ylab='mpg')
 plot(ds$cyl,ds$mpg,pch=19,col='lightgreen',xlab='cylinder', ylab='mpg')
